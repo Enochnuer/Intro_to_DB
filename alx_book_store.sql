d402d93f1a8a76e8679fe872c5f3fff5a61a5a98
Enoch Nuertey <enochnuer111@gmail.com>
2024-07-28 19:22:13 +0000

    initial commit

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,5 +1,5 @@
 
-CREATE DATABASE alx_book_store;
+CREATE DATABASE alx_book_store IF NOT EXISTS alx_book_store;
 USE alx_book_store;
 
 CREATE TABLE Books (
@@ -12,32 +12,32 @@ CREATE TABLE Books (
 );
 
 CREATE TABLE Authors (
-    author_id INT PRIMARY KEY AUTO_INCREMENT,
+    author_id INT PRIMARY KEY,
     author_name VARCHAR(215) NOT NULL
 );
 
 CREATE TABLE Customers (
-    customer_id INT PRIMARY KEY AUTO_INCREMENT,
+    customer_id INT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
     email VARCHAR(215),
     address TEXT
 );
 
 CREATE TABLE Orders (
-    order_id INT PRIMARY KEY AUTO_INCREMENT,
+    order_id INT PRIMARY KEY,
     customer_id INT,
     order_date DATE,
-    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)   
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 
 );
 
 CREATE TABLE Order_Details 
  (
-    orderdetailid INT PRIMARY KEY AUTO_INCREMENT,
+    orderdetailid INT PRIMARY KEY,
     order_id INT,
     book_id INT,
-    quantity INT,
+    quantity DOUBLE,
     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
-    FOREIGN KEY (book_id) REFERENCES Books(book_id)   
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 
 );
