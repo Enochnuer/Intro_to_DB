a09ca2d8d44f373972ee6f43aec67a9588e0960d
Enoch Nuertey <enochnuer111@gmail.com>
2024-07-28 18:58:17 +0000

    initial commit

---
+++ alx_book_store.sql
@@ -0,0 +1,50 @@
+import mysql.connector
+mydb = mysql.connector.connect(
+    host = "localhost"
+    user = "root"
+    password = "toads123."
+    database = alx_book_store
+)
+
+CREATE DATABASE alx_book_store ;
+USE alx_book_store ;
+
+CREATE TABLE Books (
+    book_id INT PRIMARY KEY ,
+    title VARCHAR(130) NOT NULL ,
+    author_id INT ,
+    price DOUBLE ,
+    publication_date DATE ,
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+);
+
+CREATE TABLE Authors (
+    author_id INT PRIMARY KEY AUTO_INCREMENT,
+    author_name VARCHAR(215) NOT NULL
+);
+
+CREATE TABLE Customers (
+    customer_id INT PRIMARY KEY AUTO_INCREMENT,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215),
+    address TEXT
+);
+
+CREATE TABLE Orders (
+    order_id INT PRIMARY KEY AUTO_INCREMENT,
+    customer_id INT,
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)   
+
+);
+
+CREATE TABLE Order_Details 
+ (
+    orderdetailid INT PRIMARY KEY AUTO_INCREMENT,
+    order_id INT,
+    book_id INT,
+    quantity INT,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)   
+
+);
